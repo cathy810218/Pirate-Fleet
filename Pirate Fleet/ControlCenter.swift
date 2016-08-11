e91e2fa6ac096eff6da6b177299ec4197b43c4a7
Cathy Oun <cathy810218@gmail.com>
2016-08-12 01:43:05 +0800

    first commit

---
+++ Pirate Fleet/ControlCenter.swift
@@ -0,0 +1,134 @@
+//
+//  ControlCenter.swift
+//  Pirate Fleet
+//
+//  Created by Jarrod Parkes on 9/2/15.
+//  Copyright © 2015 Udacity. All rights reserved.
+//
+
+struct GridLocation {
+    let x: Int
+    let y: Int
+}
+
+struct Ship {
+    let length: Int
+    let location: GridLocation
+    let isVertical: Bool
+    let isWooden: Bool
+    
+
+    var cells: [GridLocation] {
+        get {
+            let start = self.location
+            let end: GridLocation = ShipEndLocation(self)
+            var occupiedCells = [GridLocation]()
+            
+            if start.x == end.x {
+                for i in start.y...end.y {
+                    let body = GridLocation(x: start.x, y: i)
+                    occupiedCells.append(body)
+                }
+            } else if start.y == end.y {
+                for i in start.x...end.x {
+                    let body = GridLocation(x: i, y: start.y)
+                    occupiedCells.append(body)
+                }
+            }
+            return occupiedCells
+        }
+    }
+    
+    var hitTracker: HitTracker
+    var sunk: Bool {
+        get {
+            let dict = hitTracker.cellsHit
+            for eachCell in dict {
+                if eachCell.1 == false {
+                    return false
+                }
+            }
+            return true
+        }
+    }
+    
+    init(length: Int, location: GridLocation, isVertical: Bool) {
+        self.length = length
+        self.location = location
+        self.isVertical = isVertical
+        self.isWooden = false
+        self.hitTracker = HitTracker()
+    }
+    init(length: Int, location: GridLocation, isVertical: Bool, isWooden: Bool) {
+        self.length = length
+        self.location = location
+        self.isVertical = isVertical
+        self.isWooden = isWooden
+        self.hitTracker = HitTracker()
+    }
+}
+
+protocol PenaltyCell {
+    var location: GridLocation {get}
+    var penaltyText: String{get}
+    var guaranteesHit: Bool{get}
+}
+
+struct Mine: PenaltyCell {
+    let location: GridLocation
+    var penaltyText: String
+    let guaranteesHit: Bool
+}
+
+struct SeaMonster: PenaltyCell {
+    let location: GridLocation
+    var penaltyText: String
+    let guaranteesHit: Bool
+    
+}
+
+class ControlCenter {
+    
+    func placeItemsOnGrid(human: Human) {
+        
+        let smallShip = Ship(length: 2, location: GridLocation(x: 3, y: 4), isVertical: true, isWooden: true)
+        human.addShipToGrid(smallShip)
+
+        let mediumShip1 = Ship(length: 3, location: GridLocation(x: 0, y: 0), isVertical: false, isWooden: false)
+        human.addShipToGrid(mediumShip1)
+        
+        let mediumShip2 = Ship(length: 3, location: GridLocation(x: 3, y: 1), isVertical: false, isWooden: true)
+        human.addShipToGrid(mediumShip2)
+        
+        let largeShip = Ship(length: 4, location: GridLocation(x: 6, y: 3), isVertical: true, isWooden: false)
+        human.addShipToGrid(largeShip)
+        
+        let xLargeShip = Ship(length: 5, location: GridLocation(x: 7, y: 2), isVertical: true, isWooden: false)
+        human.addShipToGrid(xLargeShip)
+        
+        let mine1 = Mine(location: GridLocation(x: 6, y: 0), penaltyText: "WHAT", guaranteesHit: false)
+        human.addMineToGrid(mine1)
+        
+        let mine2 = Mine(location: GridLocation(x: 3, y: 3), penaltyText: "WHAT", guaranteesHit: false)
+        human.addMineToGrid(mine2)
+        
+        let seamonster1 = SeaMonster(location: GridLocation(x: 5, y: 6), penaltyText: "HIT YA", guaranteesHit: true)
+        human.addSeamonsterToGrid(seamonster1)
+        
+        let seamonster2 = SeaMonster(location: GridLocation(x: 2, y: 2), penaltyText: "HIT YA", guaranteesHit: true)
+        human.addSeamonsterToGrid(seamonster2)
+    }
+    
+    func calculateFinalScore(gameStats: GameStats) -> Int {
+        
+        var finalScore: Int
+        
+        let sinkBonus = (5 - gameStats.enemyShipsRemaining) * gameStats.sinkBonus
+        let shipBonus = (5 - gameStats.humanShipsSunk) * gameStats.shipBonus
+        let guessPenalty = (gameStats.numberOfHitsOnEnemy + gameStats.numberOfMissesByHuman) * gameStats.guessPenalty
+        
+        finalScore = sinkBonus + shipBonus - guessPenalty
+        
+        return finalScore
+    }
+}
